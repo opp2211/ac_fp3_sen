e08e28c5ab76ff4ccadd1514d3a7be6d7d8d02aa
Kirill Maltsev <opp.2211@gmail.com>
2024-04-01 01:04:06 +0500

    init

---
+++ src/main/java/ru/maltsev/alishevcourse/restsensorapp/RestSensorAppApplication.java
@@ -0,0 +1,13 @@
+package ru.maltsev.alishevcourse.restsensorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSensorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSensorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/maltsev/alishevcourse/restsensorapp/RestSensorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maltsev.alishevcourse.restsensorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSensorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
