ac464c3627e364c86517e3495e82bee3f8721a82
Kirill Maltsev <opp.2211@gmail.com>
2024-04-02 19:03:54 +0500

    config

--- src/main/java/ru/maltsev/alishevcourse/restsensorapp/RestSensorAppApplication.java
+++ src/main/java/ru/maltsev/alishevcourse/restsensorapp/RestSensorAppApplication.java
@@ -1,7 +1,9 @@
 package ru.maltsev.alishevcourse.restsensorapp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestSensorAppApplication {
@@ -10,4 +12,8 @@ public class RestSensorAppApplication {
 		SpringApplication.run(RestSensorAppApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
